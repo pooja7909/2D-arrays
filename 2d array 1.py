699ecd324aeaea38b6e45639208e914924f05703
pooja7909 <52460015+pooja7909@users.noreply.github.com>
2021-03-05 20:43:22 +0100

    Update 2d array 1.py

--- 2d array 1.py
+++ 2d array 1.py
@@ -1,9 +1,9 @@
 laptops = [[15,34,23,12],[12,43,45,23],[13,33,43,23],[45,23,32,44],[34,32,12,23],[32,33,12,23],[22,23,33,23],[12,23,12,23],[12,34,35,23],[18,23,40,33],[20,30,42,35]]
 t=[]
-#numrows = len(laptops)
-#numcolms = len(laptops[0])
-#print(numrows)
-#print(numcolms) 
+numrows = len(laptops)
+numcolms = len(laptops[0])
+print(numrows)
+print(numcolms) 
 def sum2(laptops):
     for row in range (0,len(laptops)):
         sum1 = 0
